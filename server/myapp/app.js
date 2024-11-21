04d9df19d5b98cfbce77e42e91877acacd3fe373
Deniz Karakay <deniz@karakay.me>
2024-11-21 21:46:22 +0000

    Add reset endpoint to reset all data

--- server/myapp/app.js
+++ server/myapp/app.js
@@ -3,9 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const db = require("./db");
 
-const bodyParser = require('body-parser');
-const User = require("./models/user");
+const bodyParser = require("body-parser");
 
 var graphRouter = require("./routes/graph");
 var usersRouter = require("./routes/users");
@@ -13,23 +13,27 @@ var sensorRouter = require("./routes/sensor");
 
 var app = express();
 
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-
 // This is to enable cross-origin access
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
   // Pass to next layer of middleware
   next();
 });
@@ -50,6 +54,18 @@ app.use("/sensor", sensorRouter);
 // Serve index.html for the root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Reset Database
+app.get("/reset", async (req, res) => {
+  console.log("Resetting database");
+  try {
+    await db.connection.dropDatabase();
+    res.send("Database reset");
+  } catch (err) {
+    console.error("Error resetting database:", err);
+    res.status(500).send("Error resetting database");
+  }
 });
 
 // catch 404 and forward to error handler
